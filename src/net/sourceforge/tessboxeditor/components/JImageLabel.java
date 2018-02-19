b66d41065fff2f062835fcfab438a8710174d08e
Nikita Skotnikov <nikita.skotnikov@gmail.com>
2018-02-19 12:53:22 +0300

    Drag boxes inside opened image bounds

--- src/net/sourceforge/tessboxeditor/components/JImageLabel.java
+++ src/net/sourceforge/tessboxeditor/components/JImageLabel.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,139 +22,144 @@ import net.sourceforge.vietocr.util.Utils;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class JImageLabel extends JLabel {
 
-    private Gui gui;
-    private TessBoxCollection boxes;
-    private JTable table;
-    private boolean boxClickAction;
-    private Rectangle pressedRect;
+  private Gui gui;
+  private TessBoxCollection boxes;
+  private JTable table;
+  private boolean boxClickAction;
+  private Rectangle pressedRect;
 
-    /** Creates a new instance of JImageLabel */
-    public JImageLabel() {
-        this.addMouseListener(new MouseAdapter() {
+  /** Creates a new instance of JImageLabel */
+  public JImageLabel() {
+    this.addMouseListener(new MouseAdapter() {
 
-          @Override
-            public void mousePressed(MouseEvent me) {
-                if (boxes == null) {
-                    return;
-                }
-
-                TessBox box = boxes.hitObject(me.getPoint());
-                if (box == null) {
-                    if (!me.isControlDown()) {
-                        boxes.deselectAll();
-                        repaint();
-                        table.clearSelection();
-                    }
-                    pressedRect = null;
-                } else {
-                    if (!me.isControlDown()) {
-                        boxes.deselectAll();
-                        table.clearSelection();
-                    }
-                    pressedRect = box.getRect();
-                    box.setSelected(!box.isSelected()); // toggle selection
-                    repaint();
-                    // select corresponding table rows
-                    boxClickAction = true;
-                    java.util.List<TessBox> boxesOfCurPage = boxes.toList(); // boxes of current page
-                    for (TessBox selectedBoxes : boxes.getSelectedBoxes()) {
-                        int index = boxesOfCurPage.indexOf(selectedBoxes);
-                        table.addRowSelectionInterval(index, index);
-                        Rectangle rect = table.getCellRect(index, 0, true);
-                        table.scrollRectToVisible(rect);
-                    }
-                    boxClickAction = false;
-                }
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                gui.updateXYData();
-            }
-        });
-
-        this.addMouseMotionListener(new MouseMotionAdapter() {
-
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                if (pressedRect != null) {
-                    Point point = e.getPoint();
-                    pressedRect.setLocation(point);
-                    repaint();
-                }
-            }
-        });
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        // automatically called when repaint
-        super.paintComponent(g);
-
+      @Override
+      public void mousePressed(MouseEvent me) {
         if (boxes == null) {
-            return;
+          return;
         }
 
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.BLUE);
-        boolean resetColor = false;
+        TessBox box = boxes.hitObject(me.getPoint());
+        if (box == null) {
+          if (!me.isControlDown()) {
+            boxes.deselectAll();
+            repaint();
+            table.clearSelection();
+          }
+          pressedRect = null;
+        } else {
+          if (!me.isControlDown()) {
+            boxes.deselectAll();
+            table.clearSelection();
+          }
+          pressedRect = box.getRect();
+          box.setSelected(!box.isSelected()); // toggle selection
+          repaint();
+          // select corresponding table rows
+          boxClickAction = true;
+          java.util.List<TessBox> boxesOfCurPage = boxes.toList(); // boxes of current page
+          for (TessBox selectedBoxes : boxes.getSelectedBoxes()) {
+            int index = boxesOfCurPage.indexOf(selectedBoxes);
+            table.addRowSelectionInterval(index, index);
+            Rectangle rect = table.getCellRect(index, 0, true);
+            table.scrollRectToVisible(rect);
+          }
+          boxClickAction = false;
+        }
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) {
+        gui.updateXYData();
+      }
+    });
+
+    this.addMouseMotionListener(new MouseMotionAdapter() {
+
+      @Override
+      public void mouseDragged(MouseEvent e) {
+        if (canDrag(e) && pressedRect != null) {
+          pressedRect.setLocation(e.getPoint());
+          repaint();
+        }
+      }
+
+      private boolean canDrag(MouseEvent e) {
+        return e.getX() >= 0 && e.getY() >= 0;
+      }
+    });
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    // automatically called when repaint
+    super.paintComponent(g);
+
+    if (boxes == null) {
+      return;
+    }
+
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.setColor(Color.BLUE);
+    boolean resetColor = false;
 //        int height = getHeight();
 
-        for (TessBox box : boxes.toList()) {
-            if (box.isSelected()) {
-                g2d.setStroke(new BasicStroke(2));
-                g2d.setColor(Color.RED);
-                resetColor = true;
-            }
-            Rectangle rect = box.getRect();
-            g2d.draw(rect);
+    for (TessBox box : boxes.toList()) {
+      if (box.isSelected()) {
+        g2d.setStroke(new BasicStroke(2));
+        g2d.setColor(Color.RED);
+        resetColor = true;
+      }
+      Rectangle rect = box.getRect();
+      g2d.draw(rect);
 //            g2d.drawRect(rect.x, height - rect.y - rect.height, rect.width, rect.height);
-            if (resetColor) {
-                g2d.setStroke(new BasicStroke(1));
-                g2d.setColor(Color.BLUE);
-                resetColor = false;
-            }
-        }
+      if (resetColor) {
+        g2d.setStroke(new BasicStroke(1));
+        g2d.setColor(Color.BLUE);
+        resetColor = false;
+      }
     }
+  }
 
-    @Override
-    public boolean contains(int x, int y) {
-        if (this.boxes != null) {
-            TessBox curBox = this.boxes.hitObject(new Point(x, y));
-            if (curBox != null) {
-                String curChrs = curBox.getChrs();
-                setToolTipText(String.format("<html><h1><font face=\"%s\" >%s</font> : %s</h1></html>", this.getFont().getName(), curChrs, Utils.toHex(curChrs)));
-            } else {
-                setToolTipText(null);
-            }
-        }
-        return super.contains(x, y);
+  @Override
+  public boolean contains(int x, int y) {
+    if (this.boxes != null) {
+      TessBox curBox = this.boxes.hitObject(new Point(x, y));
+      if (curBox != null) {
+        String curChrs = curBox.getChrs();
+        setToolTipText(String.format("<html><h1><font face=\"%s\" >%s</font> : %s</h1></html>", this.getFont().getName(), curChrs, Utils.toHex(curChrs)));
+      } else {
+        setToolTipText(null);
+      }
     }
+    return super.contains(x, y);
+  }
 
-    public void setGui(Gui gui) {
-      this.gui = gui;
-    }
+  public void setGui(Gui gui) {
+    this.gui = gui;
+  }
 
-    public void setBoxes(TessBoxCollection boxes) {
-        this.boxes = boxes;
-        repaint();
-    }
+  public void setBoxes(TessBoxCollection boxes) {
+    this.boxes = boxes;
+    repaint();
+  }
 
-    /**
-     * @param table the table to set
-     */
-    public void setTable(JTable table) {
-        this.table = table;
-    }
+  /**
+   * @param table the table to set
+   */
+  public void setTable(JTable table) {
+    this.table = table;
+  }
 
-    /**
-     * @return the boxClickAction
-     */
-    public boolean isBoxClickAction() {
-        return boxClickAction;
-    }
+  /**
+   * @return the boxClickAction
+   */
+  public boolean isBoxClickAction() {
+    return boxClickAction;
+  }
 }
